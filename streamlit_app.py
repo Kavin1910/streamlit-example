d314422cbaef45765e853dcf07e15b6c9d6cac95
Kavin N R <nrkavin2000@gmail.com>
2024-04-17 14:59:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,6 @@ import matplotlib.pyplot as plt
 df_iris = pd.read_csv('Iris.csv')
 df_iris
 
-# Heatmap
-sns.heatmap(data=df_iris.corr(), annot=True, cmap='copper', linewidths=2)
-plt.show()
-
 
 
 
